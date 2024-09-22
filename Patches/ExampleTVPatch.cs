101440a1883250c1477fc363b2b0bf4af8d0be79
Karl Holzmann <karl.holzmann@students.mq.edu.au>
2024-09-22 19:19:41 +1000

    no support for file scoped namespaces

--- Patches/ExampleTVPatch.cs
+++ Patches/ExampleTVPatch.cs
@@ -1,21 +1,23 @@
 using HarmonyLib;
 
-namespace AdamTraveller.Patches;
-
-[HarmonyPatch(typeof(TVScript))]
-public class ExampleTVPatch
+namespace AdamTraveller.Patches
 {
-    [HarmonyPatch("SwitchTVLocalClient")]
-    [HarmonyPrefix]
-    private static void SwitchTVPrefix(TVScript __instance)
+
+    [HarmonyPatch(typeof(TVScript))]
+    public class ExampleTVPatch
     {
-        /*
-         *  When the method is called, the TV will be turning off when we want to
-         *  turn the lights on and vice-versa. At that time, the TV's tvOn field
-         *  will be the opposite of what it's doing, ie it'll be on when turning off.
-         *  So, we want to set the lights to what the tv's state was
-         *  when this method is called.
-         */
-        StartOfRound.Instance.shipRoomLights.SetShipLightsBoolean(__instance.tvOn);
+        [HarmonyPatch("SwitchTVLocalClient")]
+        [HarmonyPrefix]
+        private static void SwitchTVPrefix(TVScript __instance)
+        {
+            /*
+             *  When the method is called, the TV will be turning off when we want to
+             *  turn the lights on and vice-versa. At that time, the TV's tvOn field
+             *  will be the opposite of what it's doing, ie it'll be on when turning off.
+             *  So, we want to set the lights to what the tv's state was
+             *  when this method is called.
+             */
+            StartOfRound.Instance.shipRoomLights.SetShipLightsBoolean(__instance.tvOn);
+        }
     }
 }
